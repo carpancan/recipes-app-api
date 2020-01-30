b3a6be2b627b2f840bc43e3499697004f992e7b9
Carlos Paniagua Cano <carlos.paniagua@sngular.team>
2020-01-30 13:18:36 +0100

    Refactored tags and ingredients

--- app/core/test/test_models.py
+++ app/core/test/test_models.py
@@ -56,3 +56,15 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(ingredient), ingredient.name)
+
+    def test_recipe_str(self):
+        recipe = models.Recipe.objects.create(
+            user=sample_user(),
+            title='Steak and mushroom sauce',
+            time_minute=5,
+            price=5.00
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
+
+        self.assertEqual(str(ingredient), ingredient.name)
