167cb57faa0552e91202cbf15e90c204fbf6cb92
Carlos Paniagua Cano <carlos.paniagua@sngular.team>
2020-02-04 17:58:30 +0100

    Added tests for update recipe endpoints

--- app/recipe/test/test_recipe_api.py
+++ app/recipe/test/test_recipe_api.py
@@ -142,3 +142,100 @@ class PrivateRecipeApiTest(TestCase):
 
         self.assertIn(tag1, recipe.tags.all())
         self.assertIn(tag2, recipe.tags.all())
+
+    def test_create_recipe_with_ingredients(self):
+        ingredient1 = sample_ingredient(user=self.user, name='Chocolat')
+        ingredient2 = sample_ingredient(user=self.user, name='Wine')
+
+        payload = {
+            'title': 'Avocado lime chesscake',
+            'time_minutes': 60,
+            'price': 20.00,
+            'ingredients': [ingredient1.id, ingredient2.id]
+        }
+
+        response = self.client.post(RECIPES_URL, payload)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        recipe = Recipe.objects.get(id=response.data['id'])
+        ingredients = Ingredient.objects.all()
+        self.assertEqual(ingredients.count(), 2)
+        self.assertIn(ingredient1, ingredients)
+        self.assertIn(ingredient2, ingredients)
+
+        self.assertIn(ingredient1, recipe.ingredients.all())
+        self.assertIn(ingredient2, recipe.ingredients.all())
+
+    def test_update_partial_recipe(self):
+        recipe = sample_recipe(user=self.user)
+        recipe.tags.add(sample_tag(user=self.user))
+        new_tag = sample_tag(user=self.user, name='Carrots')
+
+        payload = {
+            'title': 'Carrots sauce',
+            'tags': [new_tag.id]
+        }
+        url = detail_url(recipe.id)
+        response = self.client.patch(url, payload)
+
+        recipe.refresh_from_db()
+
+        self.assertEqual(recipe.title, payload['title'])
+        self.assertEqual(len(recipe.tags.all()), len(payload['tags']))
+
+    def test_full_update_recipe(self):
+        ingredient1 = sample_ingredient(user=self.user, name='Chocolat')
+        ingredient2 = sample_ingredient(user=self.user, name='Wine')
+
+        tag1 = Tag.objects.create(user=self.user, name='Vegan')
+        tag2 = Tag.objects.create(user=self.user, name='Dessert')
+
+        payload = {
+            'title': 'Avocado lime chesscake',
+            'time_minutes': 60,
+            'price': 20.00,
+            'ingredients': [ingredient1.id, ingredient2.id],
+            'tags': [tag1.id, tag2.id],
+            'link': 'http://localhost:8000'
+        }
+
+        response = self.client.post(RECIPES_URL, payload)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        recipe_id = response.data['id']
+
+        new_ingredient1 = sample_ingredient(user=self.user, name='Vinegar')
+        new_ingredient2 = sample_ingredient(user=self.user, name='Salt')
+
+        new_tag1 = Tag.objects.create(user=self.user, name='Meat')
+        new_tag2 = Tag.objects.create(user=self.user, name='Fish')
+
+        new_payload = {
+            'title': 'Test recipe',
+            'time_minutes': 120,
+            'price': 25.00,
+            'ingredients': [new_ingredient1.id, new_ingredient2.id],
+            'tags': [new_tag1.id, new_tag2.id],
+            'link': 'http://0.0.0.0:8000'
+        }
+        url = detail_url(recipe_id)
+        new_response = self.client.put(url, new_payload)
+
+        self.assertEqual(new_response.status_code, status.HTTP_200_OK)
+
+        recipe = Recipe.objects.get(id=recipe_id)
+        self.assertEqual(new_payload['title'], recipe.title)
+        self.assertEqual(new_payload['time_minutes'], recipe.time_minutes)
+        self.assertEqual(new_payload['price'], recipe.price)
+        self.assertEqual(new_payload['link'], recipe.link)
+        self.assertEqual(len(new_payload['ingredients']),
+                         len(recipe.ingredients.all()))
+        self.assertEqual(len(new_payload['tags']),
+                         len(recipe.tags.all()))
+
+        self.assertNotEqual(payload['title'], recipe.title)
+        self.assertNotEqual(payload['time_minutes'], recipe.time_minutes)
+        self.assertNotEqual(payload['price'], recipe.price)
+        self.assertNotEqual(payload['link'], recipe.link)
+        self.assertNotIn(payload['ingredients'][0], recipe.ingredients.all())
+        self.assertNotIn(payload['tags'][0], recipe.tags.all())
